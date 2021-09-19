e799184bf3bdcb225ced33ca42be3054c5f1b8ba
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-09-19 15:43:36 +0200

    postbase model content limits

--- Entities/ObjectBases/PostBase.cs
+++ Entities/ObjectBases/PostBase.cs
@@ -8,6 +8,8 @@ namespace Entities.ObjectBases
         [MaxLength(70, ErrorMessage = "Title can't be over 70 characters")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Post content is required")]
+        [MinLength(3, ErrorMessage = "Post has to be a least 3 characters long")]
         [MaxLength(200, ErrorMessage = "Post can't be over 200 characters")]
         public string Content { get; set; }
     }
