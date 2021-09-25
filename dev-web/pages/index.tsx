df635468ec7c9385ee2d0eb7768b0d22af3c4bed
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-09-25 19:34:55 +0200

    use nginx url

--- dev-web/pages/index.tsx
+++ dev-web/pages/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Layout } from "../components/Layout";
-import { MediumPage } from "../components/MediumPage";
+import {Layout} from "../components/Layout";
+import {MediumPage} from "../components/MediumPage";
 import useSWR from "swr";
-import { PostDto } from "../types/postDto";
+import {PostDto} from "../types/postDto";
 import axios from "axios";
 
 const fetcher = (url: string) =>
-    axios.get(`https://localhost:5001/${url}`).then((res) => res.data);
+    axios.get(`http://localhost/${url}`).then((res) => res.data);
 
 const Home: React.FC = () => {
-    var posts = useSWR<PostDto[]>("api/post", fetcher);
+    const posts = useSWR<PostDto[]>("api/post", fetcher);
 
     return (
         <Layout title={`Home`} currentPageName={`Home`}>
             <MediumPage>
                 <h1 className={`underline text-lg`}>Posts</h1>
                 {posts.data?.map((post) => (
-                    <div>
-                        <span className={`font-bold`}>{post.title}:</span> {post.content}
+                    <div key={post.id}>
+                        <span className={`font-bold`}>
+                            {post.title}
+                            {`: `}
+                        </span>
+                        {post.content}
                     </div>
                 ))}
             </MediumPage>
