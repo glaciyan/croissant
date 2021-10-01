0d708ce3d6abd7b773225f713cf8000129601f52
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-01 11:04:50 +0200

    default breakpoints

--- cweb/src/theme.tsx
+++ cweb/src/theme.tsx
@@ -4,11 +4,10 @@ import { createBreakpoints } from "@chakra-ui/theme-tools";
 const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-    "2xl": "1536px",
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
 });
 
 const colors = {
