09e6a94f5d98314c9a83bef0227cd7d06181d13f
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-01 15:06:50 +0200

    active link color

--- cweb/src/components/NavLink.tsx
+++ cweb/src/components/NavLink.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import NextLink from "next/link";
 import cn from "classnames"
+import {useRouter} from "next/router";
 
 type Props = {
     href: string,
@@ -8,7 +9,10 @@ type Props = {
     active?: boolean
 };
 
-export const NavLink: React.FC<Props> = ({children, href, rightLine, active}) => {
+export const NavLink: React.FC<Props> = ({children, href, rightLine}) => {
+    const router = useRouter();
+    const active = router.route === href;
+
     return (
         <div className={cn({
             "after:w-px after:mx-2 after:bg-gray-100 after:h-3 after:inline-block after:bg-opacity-30": rightLine,
---
+++ cweb/src/pages/testing.tsx
@@ -0,0 +1,8 @@
+import type {NextPage} from "next";
+import {Text} from "@chakra-ui/react";
+
+const Testing: NextPage = () => {
+    return (<Text>testing</Text>);
+}
+
+export default Testing;
