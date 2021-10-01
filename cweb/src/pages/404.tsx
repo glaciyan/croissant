a6cd03d6556156c1b3d2c4542c33e3da41540fb3
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-01 14:56:14 +0200

    404 and error page

---
+++ cweb/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import type {NextPage} from "next";
+import {Text} from "@chakra-ui/react";
+
+const NotFound: NextPage = () => {
+    return (
+        <Text>Not Found</Text>
+    );
+}
+
+export default NotFound;
---
+++ cweb/src/pages/_error.tsx
@@ -0,0 +1,8 @@
+import type {NextPage} from "next";
+import {Text} from "@chakra-ui/react";
+
+const Error: NextPage = () => {
+    return (<Text>Something went wrong</Text>);
+}
+
+export default Error;
