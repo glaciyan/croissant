4447368250524bc4450706f25f0180a8349da216
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-01 15:46:51 +0200

    login and sign up links

---
+++ cweb/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from "next";
+import {Text} from "@chakra-ui/react";
+import {Title} from "../components/Title";
+
+const Login: NextPage = () => {
+    return (<>
+        <Title>Login</Title>
+        <Text>Log in</Text>
+    </>);
+}
+
+export default Login;
---
+++ cweb/src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from "next";
+import {Text} from "@chakra-ui/react";
+import {Title} from "../components/Title";
+
+const SignUp: NextPage = () => {
+    return (<>
+        <Title>Sign up</Title>
+        <Text>Sign Up</Text>
+    </>);
+}
+
+export default SignUp;
