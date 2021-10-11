e115ea0512db865ba1719acacd07a014a432d052
glaciyan <30321316+glaciyan@users.noreply.github.com>
2021-10-11 14:23:31 +0200

    working register

--- cweb/src/lib/api/authApi.ts
+++ cweb/src/lib/api/authApi.ts
@@ -1,43 +1,36 @@
+import { AxiosResponse } from "axios";
 import { UserDto } from "../../types/dto/userDto";
 import { UserForLoginDto } from "../../types/dto/userForLoginDto";
-import { AxiosResponse } from "axios";
-import { apiClient, ApiResponse } from "./api";
+import { apiCall, apiClient } from "./api";
 
 export default {
-    login: async (
-        email: string,
-        password: string,
-        rememberMe: boolean
-    ): Promise<ApiResponse<UserDto>> => {
-        try {
-            const response = await apiClient.post<
-                UserForLoginDto,
-                AxiosResponse<UserDto>
-            >("api/auth/login", {
+    login: async (email: string, password: string, rememberMe: boolean) => {
+        return await apiCall<UserDto>(() =>
+            apiClient.post<UserForLoginDto, AxiosResponse<UserDto>>("api/auth/login", {
                 email,
                 password,
                 rememberMe,
-            });
+            })
+        );
+    },
 
-            return { success: true, response };
-        } catch (e) {
-            return { success: false, response: e.response };
-        }
+    register: async (username: string, email: string, password: string) => {
+        return await apiCall(() =>
+            apiClient.post("api/auth/register", {
+                username,
+                email,
+                password,
+            })
+        );
     },
 
     logout: async () => {
         await apiClient.post("api/auth/logout");
     },
 
-    me: async (): Promise<ApiResponse<UserDto>> => {
-        try {
-            const response = await apiClient.get<undefined, AxiosResponse<UserDto>>(
-                "api/users/me"
-            );
-
-            return { success: true, response };
-        } catch (e) {
-            return { success: false, response: e.response };
-        }
+    me: async () => {
+        return await apiCall<UserDto>(() =>
+            apiClient.get<undefined, AxiosResponse<UserDto>>("api/users/me")
+        );
     },
 };
--- cweb/src/pages/signup.tsx
+++ cweb/src/pages/signup.tsx
@@ -1,10 +1,19 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, HStack, useToast } from "@chakra-ui/react";
 import { PageTitle } from "../components/PageTitle";
 import { Form, Formik } from "formik";
 import { Page } from "../types/page";
 import { FocusBox } from "../components/FocusBox";
+import { FormTextInput } from "../components/form/FormTextInput";
+import React from "react";
+import authApi from "../lib/api/authApi";
+import { useRouter } from "next/router";
+import { StatusCodes } from "../lib/statusCodes";
+import { toFormikError } from "../lib/api/util";
+import * as Yup from "yup";
 
 const SignUp: Page = () => {
+    const toast = useToast();
+    const router = useRouter();
     return (
         <>
             <PageTitle>Sign up</PageTitle>
@@ -13,28 +22,83 @@ const SignUp: Page = () => {
                     Let's get you started
                 </Heading>
                 <Formik
-                    initialValues={{ username: "", email: "", password: "" }}
-                    onSubmit={(values, actions) => {
-                        setTimeout(() => {
-                            alert("sign up");
-                            actions.setSubmitting(false);
-                        }, 1000);
+                    initialValues={{
+                        username: "",
+                        email: "",
+                        password: "",
+                        confirmPassword: "",
+                    }}
+                    validationSchema={Yup.object().shape({
+                        username: Yup.string().required("Enter a username"),
+                        email: Yup.string()
+                            .email("Enter a valid email")
+                            .required("An email is required"),
+                        password: Yup.string()
+                            .required("Enter a password")
+                            .min(6, "Password must be at least 6 characters"),
+                        confirmPassword: Yup.string()
+                            .oneOf([Yup.ref("password"), null], "Password does not match")
+                            .required("Confirm your password"),
+                    })}
+                    validateOnChange={false}
+                    validateOnBlur={true}
+                    onSubmit={async (values, actions) => {
+                        const { success, response } = await authApi.register(
+                            values.username,
+                            values.email,
+                            values.password
+                        );
+
+                        if (success) {
+                            toast({
+                                title: "Account created!",
+                                description:
+                                    "Your account has been created now you just need to log in",
+                                status: "success",
+                                duration: 5000,
+                                isClosable: true,
+                                position: "top",
+                            });
+
+                            router.push("/login");
+                        } else if (
+                            response.status === StatusCodes.Status422UnprocessableEntity
+                        ) {
+                            actions.setErrors(toFormikError(response.data));
+                        } else {
+                            toast({
+                                title: "Creating account failed",
+                                description:
+                                    "Something went wrong when you created your account",
+                                status: "error",
+                                duration: 5000,
+                                isClosable: true,
+                                position: "top",
+                            });
+                        }
                     }}
                 >
                     {({ isSubmitting }) => (
                         <Form>
-                            {/*<UsernameInput />*/}
-                            {/*<EmailInput mt={4} />*/}
-                            {/*<Flex mt={4} direction={["column", "row"]}>*/}
-                            {/*    <PasswordInput />*/}
-                            {/*    <GeneralInput*/}
-                            {/*        ml={[0, 4]}*/}
-                            {/*        mt={[2, 0]}*/}
-                            {/*        nameId={"confirmPassword"}*/}
-                            {/*        descriptor={"Confirm"}*/}
-                            {/*        inputProps={{ type: "password" }}*/}
-                            {/*    />*/}
-                            {/*</Flex>*/}
+                            <FormTextInput name="username" placeholder="Username" />
+                            <FormTextInput
+                                mt={4}
+                                name="email"
+                                placeholder="Email"
+                                type="email"
+                            />
+                            <HStack mt={4} alignItems="start">
+                                <FormTextInput
+                                    name="password"
+                                    placeholder="Password"
+                                    type="password"
+                                />
+                                <FormTextInput
+                                    name="confirmPassword"
+                                    placeholder="Confirm"
+                                    type="password"
+                                />
+                            </HStack>
 
                             <Button
                                 mt={12}
